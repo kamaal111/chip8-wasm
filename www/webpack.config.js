70e155aae14b970bd6b3e2a60538063ad2216fdb
Kamaal Farah <kamaal.f1@gmail.com>
2023-11-12 20:41:52 +0000

    Using TypeScript in www

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -4,6 +4,18 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./bootstrap.js",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",

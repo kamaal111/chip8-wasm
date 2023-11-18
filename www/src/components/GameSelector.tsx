4f30d7feb6e6cf7a062ef87364b091d7f0008032
Kamaal Farah <kamaal.f1@gmail.com>
2023-11-18 14:36:46 +0000

    Loading game data

--- www/src/components/GameSelector.tsx
+++ www/src/components/GameSelector.tsx
@@ -1,21 +1,25 @@
 import * as React from "react";
 
+type Game = { name: string; data: ArrayBuffer };
+
 const GAMES = ["PONG"];
 
 function GameSelector() {
-  const [games, setGames] = React.useState<Array<string>>([]);
+  const [games, setGames] = React.useState<Array<Game>>([]);
 
   React.useEffect(() => {
     loadGames();
   }, []);
 
   async function loadGames() {
-    const validGames: Array<string> = [];
+    const validGames: Array<Game> = [];
     for (const game of GAMES) {
       const response = await fetch(`games/${game}`);
       if (!response.ok) continue;
 
-      validGames.push(game);
+      const responseBlob = await response.blob();
+      const responseBlobBuffer = await responseBlob.arrayBuffer();
+      validGames.push({ name: game, data: responseBlobBuffer });
     }
 
     setGames(validGames);
@@ -25,10 +29,10 @@ function GameSelector() {
     <>
       <label>Choose a game:</label>
       <select name="games" id="games">
-        {games.map((game) => {
+        {games.map(({ name }) => {
           return (
-            <option value={game} key={game}>
-              {game}
+            <option value={name} key={name}>
+              {name}
             </option>
           );
         })}
